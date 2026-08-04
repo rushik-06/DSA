5cbd5b4c0742aa97b35b6c176924a0f8d413a7df
Rushikesh Mali <rushikeshnmali2006@gmail.com>
2026-08-04 22:59:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (54.39%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int low = 0;
+        int high = n - 1;
+
+        while (low < high) {
+            if (!isalnum(s[low])) {
+                low++;
+                continue;
+            }
+            if (!isalnum(s[high])) {
+                high--;
+                continue;
+            }
+            if (tolower(s[low]) != tolower(s[high])) {
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+};
